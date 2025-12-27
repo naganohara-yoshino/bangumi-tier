4b5bc49187b8d5d743ac254a91525323e29ab3a7
naganohara-yoshino <naganohara-yoshino@outlook.com>
2025-12-28 07:18:53 +0800

    feat: home page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div
+    class="flex flex-col-reverse min-w-screen overflow-clip bg-gray-300 lg:flex-row-reverse lg:min-h-screen lg:h-screen"
+>
+    <div
+        class="flex flex-col p-8 justify-center items-center w-full lg:h-full lg:w-2/5 bg-white"
+    >
+        <button
+            class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            on:click={() => alert("Button clicked!")}
+        >
+            Click Me
+        </button>
+        <button
+            class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            on:click={() => alert("Button clicked!")}
+        >
+            Click Me
+        </button>
+        <button
+            class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            on:click={() => alert("Button clicked!")}
+        >
+            Click Me
+        </button>
+    </div>
+    <div class="flex flex-1 justify-center p-8">
+        <p>Main Content Area</p>
+    </div>
+</div>
