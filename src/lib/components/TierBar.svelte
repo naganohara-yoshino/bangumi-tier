5d01f030dac6aa5fb7174bb0ce28f9d20a2860fc
naganohara-yoshino <naganohara-yoshino@outlook.com>
2025-12-28 08:28:06 +0800

    feat: TierBar and ItemList

---
+++ src/lib/components/TierBar.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { flip } from "svelte/animate";
+  import { dndzone } from "svelte-dnd-action";
+  let { items } = $props();
+  const flipDurationMs = 300;
+  function handleDndConsider(e) {
+    items = e.detail.items;
+  }
+  function handleDndFinalize(e) {
+    items = e.detail.items;
+  }
+</script>
+
+<div
+  class="min-w-10 h-16 flex bg-gray-200 rounded-lg p-2"
+  use:dndzone={{ items, flipDurationMs }}
+  on:consider={handleDndConsider}
+  on:finalize={handleDndFinalize}
+>
+  {#each items as item (item.id)}
+    <div
+      class="flex min-w-10 h-10 p-3 border-2 border-blue-500 m-1 text-center items-center justify-center bg-white rounded shadow cursor-move select-none"
+      animate:flip={{ duration: flipDurationMs }}
+    >
+      {item.name}
+    </div>
+  {/each}
+</div>
---
+++ src/routes/tier/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+  import ItemList from "$lib/components/ItemList.svelte";
+  import TierBar from "$lib/components/TierBar.svelte";
+
+  let tierItems1 = [
+    { id: 1, name: "Tier 1" },
+    { id: 2, name: "Tier 2" },
+    { id: 3, name: "Tier 3" },
+  ];
+  let tierItems2 = [
+    { id: 11, name: "Tier 11" },
+    { id: 22, name: "Tier 22" },
+    { id: 33, name: "Tier 33" },
+  ];
+</script>
+
+<div
+  class="flex flex-col-reverse min-w-screen overflow-clip bg-gray-300 lg:flex-row-reverse lg:min-h-screen lg:h-screen"
+>
+  <div
+    class="flex flex-col p-8 justify-center items-center w-full lg:h-full lg:w-2/5 bg-white"
+  >
+    <ItemList
+      items={[
+        { id: "a", name: "Item A" },
+        { id: "b", name: "Item B" },
+        { id: "c", name: "Item C" },
+        { id: "d", name: "Item D" },
+        { id: "e", name: "Item E" },
+      ]}
+    />
+  </div>
+  <div class="flex flex-1 flex-col justify-center items-center p-8">
+    <TierBar items={tierItems1} />
+    <TierBar items={tierItems2} />
+  </div>
+</div>
