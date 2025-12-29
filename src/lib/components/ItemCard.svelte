9015f3679872df7866be12f361b8372a3fe897d4
naganohara-yoshino <naganohara-yoshino@outlook.com>
2025-12-29 09:20:27 +0800

    feat: Better ItemCard

--- src/lib/components/ItemCard.svelte
+++ src/lib/components/ItemCard.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
+  import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
+  } from "$lib/components/ui/card/index.js";
   import type { Item } from "$lib/schemas/item";
   let { item }: { item: Item } = $props();
 </script>
 
-<div class="h-64 w-48 rounded-lg shadow-lg overflow-hidden relative">
-  <img
-    src={item.image}
-    alt={item.name}
-    class="absolute inset-0 w-full h-full object-cover"
-  />
+<Card class="m-px h-24 w-18 rounded-lg shadow-lg overflow-hidden relative">
+  <CardContent>
+    <img
+      src={item.image}
+      alt={item.name}
+      class="absolute inset-0 w-full h-full object-cover object-top"
+    />
+  </CardContent>
 
-  <div
-    class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4"
-  >
-    <h2 class="text-lg font-bold relative z-10">{item.name}</h2>
-  </div>
-</div>
+  <CardFooter class="absolute bottom-0  w-full p-1">
+    <CardTitle
+      class="w-full text-center text-[10px] text-pretty antialiased drop-shadow-md"
+      >{item.name}</CardTitle
+    >
+  </CardFooter>
+</Card>
