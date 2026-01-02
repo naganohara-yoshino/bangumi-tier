9907cc4cd45fdec04496075965cf892f23c6fc44
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-01-02 09:56:47 +0800

    feat: "redirecting"

---
+++ src/routes/index/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { addIndexAndGoto } from "$lib/actions.svelte";
+    import { page } from "$app/state";
+
+    onMount(() => {
+        addIndexAndGoto(Number(page.params.id));
+    });
+</script>
+
+<h1 class="prose">Redirecting...</h1>
+<p class="prose">Wait a moment.</p>
