ed6178cd8751c1c6f475c7369ae1901ac6eed413
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-01-02 10:04:40 +0800

    feat: i18n for redirecting page

--- src/routes/index/[id]/+page.svelte
+++ src/routes/index/[id]/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import { addIndexAndGoto } from "$lib/actions.svelte";
     import { page } from "$app/state";
+    import { m } from "$lib/paraglide/messages";
 
     onMount(() => {
         addIndexAndGoto(Number(page.params.id));
@@ -9,4 +10,4 @@
 </script>
 
 <h1 class="prose">Redirecting...</h1>
-<p class="prose">Wait a moment.</p>
+<p class="prose">{m.please_wait()}</p>
